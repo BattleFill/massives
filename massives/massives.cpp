1d80734e94c671c21649d88237797045397b8ebc
Battle Fill <egorkuznecov283@gmail.com>
2024-02-21 12:52:17 +0300

    Добавьте файлы проекта.

---
+++ massives/massives.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+using namespace std;
+int main() {
+    setlocale(LC_ALL, "RUS");
+    const int stroki = 3;
+    const int ll = 5;
+    double array[stroki][ll];
+    cout << "Элементы массива:\n";
+    for (int i = 0; i < stroki; ++i) {
+        cout << "Строка " << i + 1 << " ";
+        for (int j = 0; j < ll; ++j) {
+            cout << "Элемент " << j + 1 << ": ";
+            cin >> array[i][j];
+        }
+    }
+    cout << "Средние арифметические:\n";
+    for (int i = 0; i < stroki; ++i) {
+        double sum = 0.0;
+        for (int j = 0; j < ll; ++j) {
+            sum += array[i][j];
+        }
+        double average = sum / ll;
+        cout << "Строка " << i + 1 << ": " << average << endl;
+    }
+    return 0;
+}
